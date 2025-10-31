98e89e167097be40f21f22b5e0464ff819b4e74b
suhaas3 <suhaas3w@gmail.com>
2025-10-31 17:02:36 +0530

    installing and setup the tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import "./index.css";
+
 
 function App() {
 
   return (
     <>
-    
-      <h1>Hello API project</h1>
+    <div className="pt-4">
+       <div class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+        <img class="size-12 shrink-0" src="/img/logo.svg" alt="ChitChat Logo" />
+        <div>
+          <div class="text-xl font-medium text-black dark:text-white">hiiii</div>
+          <p class="text-gray-500 dark:text-gray-400">You have a new message!</p>
+        </div>
+      </div>
+
+    </div>
+     
     </>
   )
 }
